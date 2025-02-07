b7ed7fcc7df51446ea91f43374837c19c57002e1
Daniele <Daniele@Atomos>
2025-02-07 15:13:13 +0100

    Added Sphinx documentation to the public documentation branch.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,6 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.0-beta5',
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
